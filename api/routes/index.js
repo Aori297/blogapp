1acb6a8bae116fead678450f3a43317e6382fb7e
Aori297 <shaswats.snm@gmail.com>
2024-07-17 11:36:44 +0545

    upload text and images for blogs with multer, streamifier and cloudinary

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./auth.routes.js";
+import blogRoutes from "./blog.routes.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/blog", blogRoutes);
 
 export default router;
