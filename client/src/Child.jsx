33834dc9045a0fd458c1fc4ec05aa2ee11514c73
Aori297 <shaswats.snm@gmail.com>
2024-07-26 10:50:21 +0545

    react learning

---
+++ client/src/Child.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Child = ({heading, subheading}) => {
+  return (
+    <div>
+      <p>{heading}</p>
+      <p>{subheading}</p>
+    </div>
+  )
+}
+
+export default Child
