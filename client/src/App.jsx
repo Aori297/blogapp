0e2117aa62b50eec810b295cb2c7bd47f45d0acb
Aori297 <shaswats.snm@gmail.com>
2024-07-29 10:57:37 +0545

    reacc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import Child from './Child'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: "",
+    age: "",
+    email: "",
+  })
 
-  const h1 = "Hello World"
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
   return (
-    <React.Fragment>
-      <Child 
-        heading ="Child" 
-        subheading ="uhm"
-        para ="paragraph!"
-        /> 
-
-        <button onClick={() => alert("Clicked")}>Click Me</button>
-    </React.Fragment>
-    
+    <>
+      {JSON.stringify(user)}
+      <form onSubmit={e => {}}>
+        <input 
+          type="text" 
+          name="name" 
+          value={user.name} 
+          onChange={handleChange}
+        />
+        <input 
+          type="number" 
+          name="age" 
+          value={user.age} 
+          onChange={handleChange}
+        />
+        <input 
+          type="email" 
+          name="email" 
+          value={user.email} 
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
   )
+  
 }
 
 export default App
