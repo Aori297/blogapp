dc49ab6f2c856f3f090da0cf4eb5bec0744e6a3f
Aori297 <shaswats.snm@gmail.com>
2024-08-05 10:59:54 +0545

    same

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='font-play text-3xl font-bold'>Welcome Back!</h1>
+
+      <form className="mt-10"> </form>
+    </div>
+  )
+}
+
+export default LoginPage
